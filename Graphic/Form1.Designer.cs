fb4bd1daf555c4bc71ea15c1aee1e8af907d060d
wulingli-nexthub <3499279603@qq.com>
2025-12-12 17:22:02 +0800

    RCS Ver 2.5.0  拆分类前

--- Graphic/Form1.Designer.cs
+++ Graphic/Form1.Designer.cs
@@ -114,7 +114,7 @@
             this.Controls.Add(this.labelAcc);
             this.Controls.Add(this.btnReset);
             this.Name = "Form1";
-            this.Text = " RCS Ver2.4.7 吴灵丽";
+            this.Text = " RCS Ver2.5.0 吴灵丽";
             ((System.ComponentModel.ISupportInitialize)(this.numericAcc)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericVinit)).EndInit();
             this.ResumeLayout(false);
