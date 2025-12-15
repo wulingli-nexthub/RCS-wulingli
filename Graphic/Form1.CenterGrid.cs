dcd553c34c06d56f4f7d1c52cade64ac4dc47d8c
wulingli-nexthub <3499279603@qq.com>
2025-12-15 17:16:31 +0800

    优化拆分

--- Graphic/Form1.CenterGrid.cs
+++ Graphic/Form1.CenterGrid.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Graphic
 {
@@ -46,6 +48,71 @@ namespace Graphic
             this.Invalidate();   // 重绘
         }
 
+        /// <summary>
+        /// 鼠标滚轮缩放
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // 当前鼠标的屏幕坐标、对应的世界坐标
+            var mouseScreen = new PointF(e.X, e.Y);
+            var mouseWorldBefore = ScreenToWorld(mouseScreen.X, mouseScreen.Y);
+
+            // 计算新的缩放
+            double zoomFactor = (e.Delta > 0) ? 1.1 : 0.9;            //e.Delta为鼠标滚动，向上大于0
+            double newScale = _scale * zoomFactor;                    //缩放后记得修改像素
+
+            // 限制缩放范围
+            if (newScale < 5) newScale = 5;       // 最小
+            if (newScale > 200) newScale = 200;   // 最大
+
+            // 调整 offset 使缩放中心为鼠标所在点
+            _scale = newScale;
+            // 使世界坐标 mouseWorldBefore 仍然映射到原来的 mouseScreen
+            _offsetX = mouseScreen.X - mouseWorldBefore.X * _scale;
+            _offsetY = mouseScreen.Y - mouseWorldBefore.Y * _scale;
+
+            this.Invalidate(); // 触发重绘
+        }
+
+        // 鼠标按下：准备拖动
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _isPanning = true;
+                _lastMousePos = e.Location;         //获取当前鼠标位置
+                this.Cursor = Cursors.Hand;
+            }
+        }
+
+        // 鼠标移动：拖动画布
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isPanning)
+            {
+                int dx = e.X - _lastMousePos.X;          //计算位移
+                int dy = e.Y - _lastMousePos.Y;
+                _lastMousePos = e.Location;
+
+                _offsetX += dx;                          //拖动后修改起点坐标
+                _offsetY += dy;
+
+                this.Invalidate(); // 重绘
+            }
+        }
+
+        // 鼠标松开：结束拖动
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _isPanning = false;
+                this.Cursor = Cursors.Default;
+            }
+        }
+
         private void Form1_Resize(object sender, EventArgs e)
         {
             int clientWidth = this.ClientSize.Width;
@@ -63,6 +130,28 @@ namespace Graphic
             _offsetY = (clientHeight - gridPixelHeight) / 2.0;
 
             this.Invalidate();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // 将当前缩放和偏移恢复到初始值
+            _scale = _initialScale;
+
+            int clientWidth = this.ClientSize.Width;
+            int clientHeight = this.ClientSize.Height;
+
+            if (clientWidth <= 0 || clientHeight <= 0)
+                return;
+
+            // 当前缩放下，整个世界网格区域在屏幕上的像素宽高
+            double gridPixelWidth = _worldWidthM * _scale;
+            double gridPixelHeight = _worldHeightM * _scale;
+
+            // 让左上角偏移重新计算成居中
+            _offsetX = (clientWidth - gridPixelWidth) / 2.0;
+            _offsetY = (clientHeight - gridPixelHeight) / 2.0;
+
+            this.Invalidate(); // 触发重绘
         }
     }
 }
