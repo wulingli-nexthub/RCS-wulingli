6bcf9b931c5afc66f0084c3bcc399533febba905
wulingli-nexthub <3499279603@qq.com>
2025-12-24 09:34:14 +0800

    111

--- Graphic/Form1.Initialize.cs
+++ Graphic/Form1.Initialize.cs
@@ -142,6 +142,13 @@ namespace Graphic
                 dt: _dt
             );
             _robotSimulator._Thead_Start();
+
+            _destinationPicker = new DestinationPicker(
+                transform: _worldTransform,
+                navigator: _robotAutoNavigator,
+                getWorldWidthM: () => _worldWidthM,
+                getWorldHeightM: () => _worldHeightM,
+                cellSizeM: CellSizeM);
         }
     }
 }
